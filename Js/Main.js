60e8aa769296782829c92145fa48008c0a962883
alirezasamadias <alireza.samadi.as.as@gmail.com>
2023-12-20 07:27:31 +0330

    edit

--- Js/Main.js
+++ Js/Main.js
@@ -52,14 +52,14 @@ document.onreadystatechange = () => {
         // home background
         const backgrounds = ['Burger-Large.jpg','Coffee-Large.jpg','Fruit-Large.jpg','Pasta-Large.jpg'];
         let num = 0;
-        bgSlide.style.backgroundImage = `url(../Images/${backgrounds[num]})`;
+        bgSlide.style.backgroundImage = `url(Images/${backgrounds[num]})`;
         setInterval(() => {
             num++;
             if (num > backgrounds.length - 1) {
                 num = 0;
             }
 
-            bgSlide.style.backgroundImage = `url(../Images/${backgrounds[num]}) `;
+            bgSlide.style.backgroundImage = `url(Images/${backgrounds[num]}) `;
         },5000);
     }
 };
